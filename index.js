b6efd5840e9b667d30c8f52df9eb0941993be874
F4ulty12 <149839830+F4ulty12@users.noreply.github.com>
2026-07-19 15:27:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+const { 
+  Client, 
+  GatewayIntentBits, 
+  ActivityType,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} = require("discord.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ],
 });
 
 client.once("ready", () => {
@@ -15,6 +25,43 @@ client.once("ready", () => {
         type: ActivityType.Watching,
       },
     ],
+  });
+});
+
+client.on("guildMemberAdd", async (member) => {
+  const channel = member.guild.channels.cache.get("1528483105151783043");
+
+  if (!channel) {
+    console.log("Welcome channel not found.");
+    return;
+  }
+
+  const memberCount = member.guild.memberCount;
+
+  const memberButton = new ButtonBuilder()
+    .setCustomId("member_count")
+    .setLabel(`${memberCount}`)
+    .setEmoji("<:person:1528473741045141745>")
+    .setStyle(ButtonStyle.Secondary)
+    .setDisabled(true);
+
+  const informationButton = new ButtonBuilder()
+    .setLabel("Information ↗️")
+    .setStyle(ButtonStyle.Link)
+    .setURL(
+      "https://discord.com/channels/1225971370681438281/1226724471348531290"
+    );
+
+  const row = new ActionRowBuilder()
+    .addComponents(
+      memberButton,
+      informationButton
+    );
+
+  await channel.send({
+    content:
+      `Welcome <@${member.id}> to **Kentucky State Roleplay**. We hope you have a fantastic time here. You are member \`#${memberCount}\`.`,
+    components: [row]
   });
 });
 
